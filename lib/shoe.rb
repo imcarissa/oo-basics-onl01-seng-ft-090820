7a838e61942bf77f42791d067098d79627ae3afb
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-19 23:35:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,5 +1,39 @@
 class Shoe
   
+  def brand(title)
+    @title = title
+  end
+  
+  def title
+    @title
+  end
+  
+  def color=(author)
+    @author = author
+  end
+  
+  def author
+    @author
+  end
+  
+  def size
+    @page_count
+  end
+  
+  def page_count
+    @page_count
+  end
+  
+  def material
+    @genre = 
+  end
+  
+  def genre
+    @genre
+    
+  def condition
+    @conditon
+  end
 end
 
 shoe = Shoe.new("Adidas")
