a14681135b5886462ca6aeada3dc5b9a7daa7f06
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-20 00:04:59 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,13 +7,13 @@ class Shoe
     @brand = brand
   end
   
-  #def brand
-   # @brand
-  end
+  # def brand
+  #   @brand
+  # end
   
-  def color=(color)
-    @color = color
-  end
+  # def color=(color)
+  #   @color = color
+  # end
   
   def color
     @color
